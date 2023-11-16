46b865da0a189ec5e38d0f3776f32f8db1f85a43
ynnij <ynnnij@gmail.com>
2023-11-16 15:59:39 +0900

    init

---
+++ src/main/java/com/vitallog/VitalLogApplication.java
@@ -0,0 +1,13 @@
+package com.vitallog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitalLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitalLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vitallog/VitalLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitallog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitalLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
